47b616d7e7d5e344e32ffda6a98c6679ac5338b9
Algirdas Rascius <arascius@kayak.com>
2013-10-02 14:41:04 +0300

    Updated DB schema to use auto-incremented promary keys. Sample data population SQL statements extracted to separate sample-data.sql file.

--- conference/src/main/resources/sql/schema.sql
+++ conference/src/main/resources/sql/schema.sql
@@ -1,27 +1,14 @@
-create table demo (
+CREATE TABLE demo (
 	id IDENTITY PRIMARY KEY,
     data VARCHAR(100)
 );
 
-INSERT INTO demo (data) VALUES ('Hello, world!');
-INSERT INTO demo (data) VALUES ('Hello, world again!');
-INSERT INTO demo (data) VALUES ('Hello, world for the third time!');
-
-
-create table conference (
-    id int,
-    name VARCHAR(100) not null,
-    startDate datetime not null,
-    endDate datetime not null,
-    PRIMARY KEY (id)
+CREATE TABLE conference (
+    id IDENTITY PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    startDate DATETIME NOT NULL,
+    endDate DATETIME NOT NULL
 );
 
-create index startDate on conference (startDate);
-create index endDate on conference (endDate);
-
-INSERT INTO conference (id, name, startDate, endDate) VALUES (1, 'NFQ Akademijos atidarymas. Įvadas į šiuolaikinio programuotojo darbą', '2013-09-30 17:00:00', '2013-09-30 18:00:00');
-INSERT INTO conference (id, name, startDate, endDate) VALUES (2, 'Projekto valdymas ir Agile', '2013-10-02 17:00:00', '2013-10-02 18:00:00');
-INSERT INTO conference (id, name, startDate, endDate) VALUES (3, 'J2EE programavimui naudojami įrankiai', '2013-10-07 17:00:00', '2013-10-07 18:00:00');
-INSERT INTO conference (id, name, startDate, endDate) VALUES (4, 'Web puslapių vartotojo sąsajos kūrimas (1-a dalis)', '2013-10-09 17:00:00', '2013-10-09 18:00:00');
-
-COMMIT;
+CREATE INDEX startDate ON conference (startDate);
+CREATE INDEX endDate ON conference (endDate);
