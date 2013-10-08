8d54cad9ea17a88ceca4ef23aee5943e63432692
Arunas Miliunas <amiliunas@kayak.com>
2013-10-08 10:11:28 +0300

    Added ids to avoid data duplication.

--- conference/src/main/resources/sql/sample-data.sql
+++ conference/src/main/resources/sql/sample-data.sql
@@ -1,11 +1,11 @@
 
-INSERT INTO demo (data) VALUES ('Hello, world!');
-INSERT INTO demo (data) VALUES ('Hello, world again!');
-INSERT INTO demo (data) VALUES ('Hello, world for the third time!');
+INSERT INTO demo (id, data) VALUES (1, 'Hello, world!');
+INSERT INTO demo (id, data) VALUES (2, 'Hello, world again!');
+INSERT INTO demo (id, data) VALUES (3, 'Hello, world for the third time!');
 
-INSERT INTO conference (name, startDate, endDate) VALUES ('NFQ Akademijos atidarymas. Įvadas į šiuolaikinio programuotojo darbą', '2013-09-30 17:00:00', '2013-09-30 18:00:00');
-INSERT INTO conference (name, startDate, endDate) VALUES ('Projekto valdymas ir Agile', '2013-10-02 17:00:00', '2013-10-02 18:00:00');
-INSERT INTO conference (name, startDate, endDate) VALUES ('J2EE programavimui naudojami įrankiai', '2013-10-07 17:00:00', '2013-10-07 18:00:00');
-INSERT INTO conference (name, startDate, endDate) VALUES ('Web puslapių vartotojo sąsajos kūrimas (1-a dalis)', '2013-10-09 17:00:00', '2013-10-09 18:00:00');
+INSERT INTO conference (id, name, startDate, endDate) VALUES (1, 'NFQ Akademijos atidarymas. Įvadas į šiuolaikinio programuotojo darbą', '2013-09-30 17:00:00', '2013-09-30 18:00:00');
+INSERT INTO conference (id, name, startDate, endDate) VALUES (2, 'Projekto valdymas ir Agile', '2013-10-02 17:00:00', '2013-10-02 18:00:00');
+INSERT INTO conference (id, name, startDate, endDate) VALUES (3, 'J2EE programavimui naudojami įrankiai', '2013-10-07 17:00:00', '2013-10-07 18:00:00');
+INSERT INTO conference (id, name, startDate, endDate) VALUES (4, 'Web puslapių vartotojo sąsajos kūrimas (1-a dalis)', '2013-10-09 17:00:00', '2013-10-09 18:00:00');
 
 COMMIT;
